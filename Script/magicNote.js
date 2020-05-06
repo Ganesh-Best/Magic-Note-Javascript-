2090bf1e20696ff5c5a88ec2fc9ae369c42a8218
Ganesh-Best <=>
2020-05-06 20:53:13 +0530

    4th Commit

--- Script/magicNote.js
+++ Script/magicNote.js
@@ -1,4 +1,7 @@
+/* window.localStorage is a object provided by javascript Engine to store data:
 
+
+*/
 //Selecting element Object having Id value : addNote and storing to addButton;
 const addButton = document.querySelector('#addNote');
 
@@ -26,21 +29,25 @@ search.addEventListener('input',eventObject => {
   //Children : child elements of that object :
   let elements =  document.querySelector('.row').children;
     
-  //Array.from function convert HTMLCollection object to Array object:
+  //Array.from() function convert HTMLCollection object to Array object:
   //so that we can apply for each :
   //forEach is callback function :,it takes a function as arguments :
+  //I am passing an arrow function to forEach function:
       Array.from(elements).forEach(element =>{
-              
-         let p  = element.querySelector('p');                
+        //It selects child paragraph element whom to element object point & store to p variable/object :                 
+         let p  = element.querySelector('p'); 
+        
+        //It converts text of paragraph object to lowercase and store to data variable : 
          let data  = p.innerText.toLowerCase() ;
-         console.log(data.toLowerCase());
-         console.log(searchData.toLowerCase());
-
+        
+        //If block execute if searchData not found in data :
+        //includes is a function which receive string as argument and  return true of searchData available in data: 
          if( !(data.includes(searchData)))
-              element.style.display = "none";
-
+              element.style.display = "none"; //It will hide element
+       
+       //If block will execute if searchData has emptyString :       
         if(searchData == "")
-           element.style.display = "block"; 
+           element.style.display = "block"; //It will show elememt :
               
       });
    
@@ -85,42 +92,72 @@ addButton.addEventListener('click', eventObject => {
     //Storing JSON Object to Notes key in localStorage Object : 
     window.localStorage.setItem('Notes', Notes);
    
-    
+    //calling displayNote function :
     displayNote();
     
 });
 
-const deleteNote = targetElement =>{
+//It is deleteNote arrow function:It is responsible to deleting note from page : 
+//It automatically call when  user click to delete note button :because I did inline event handling :
 
+const deleteNote = targetElement =>{
+//targetElement a object which is point to element whom to this event bind:
+
+//first it select parent Object of target Object(Element) :
+//Then findout paragraph element among their child:
+//and select id of paragraph element and store to Id variable : 
 let Id = targetElement.parentNode.querySelector('p').id ;
-     
+
+
+
+    // It checks whether Notes key present on localStorage Object or not :
+    //If not present , return null:
+    //If Present it return  its corresponding value : 
+ 
       let Notes  = window.localStorage.getItem('Notes');
+    
+   //It  convets into JSON object to javascript Object : 
          Notes = JSON.parse(Notes);
          
-        
+    //Iterate javascript object :    
          for(let key in Notes){
-                   
-             if(key === Id )
-              delete Notes[Id];
-         }
             
+           //It key of NoteObject match with Id variable: 
+           //then it will delete particular key,valye pair from Note Object: 
+             if(key === Id )
+              delete Notes[Id]; // delete particular key,valye pair from Note Object: 
+         }
+        
+        //convert Notes javascript Object to JSON Object and store to localStorage : 
        window.localStorage.setItem('Notes',JSON.stringify(Notes));
-
+ 
+    //Calling display Note function:   
     displayNote();
-    console.log(localStorage);
+   
 }
 
 //It is displayNote function :responsible to display notes :
 const displayNote = () => {
   
-   //
+    // It checks whether Notes key present on localStorage Object or not :
+    //If not present , return null:
+    //If Present it return  its corresponding value : 
    let  Notes = window.localStorage.getItem('Notes');
+
+   //Convert JSON  Object to Javascript Object & store to Notes:
     Notes = JSON.parse(Notes);
+
+  //create an element string to text variable :  
     let text = "";
   
+   //It execute If block instructions if Notes contain null : 
+   //Otherwise it execute Else Block :
+
     if(!Notes)
        text = "<h1>Oops No Notes Found :</h1>";         
     else{
+      //It iterates Notes object 
+      //for Particular Key and value it use a templete and cancatinate with text variable:  
          for(let key in Notes){
            
           text += `
@@ -131,10 +168,12 @@ const displayNote = () => {
                 <button  class="btn btn-primary w-100" id ="deleteNote" onclick = deleteNote(this) >Delete Note</button>
                </div>
             </div>
-      `     
+      `
+     //onclick = deleteNote(this) :It is  traditional way to handle event : & here this pointing to button element: 
+     //deleteNote() function will run automatically whenever user click Delete Note Button :     
    }
 }   
-  
+ //It selects Object (element having class value : row) and assign text to its inner Html :
  document.querySelector('.row').innerHTML = text;
 } 
 //Calling Display note function :
